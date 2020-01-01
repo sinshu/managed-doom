9c141f918e8b6fe165a54233c5f1f20acf2e1e33
田中信秋 <sinshu.d@gmail.com>
2020-01-02 01:48:36 +0900

    Add state info.

---
+++ ManagedDoom/src/StateDef.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ManagedDoom
+{
+    public class StateDef
+    {
+        private Sprite sprite;
+        private int frame;
+        private int tics;
+        private Action action;
+        private State next;
+        private int misc1;
+        private int misc2;
+
+        public StateDef(
+            Sprite sprite,
+            int frame,
+            int tics,
+            Action action,
+            State next,
+            int misc1,
+            int misc2)
+        {
+            this.sprite = sprite;
+            this.frame = frame;
+            this.tics = tics;
+            this.action = action;
+            this.next = next;
+            this.misc1 = misc1;
+            this.misc2 = misc2;
+        }
+
+        public Sprite Sprite => sprite;
+        public int Frame => frame;
+        public int Tics => tics;
+        public Action Action => action;
+        public State Next => next;
+        public int Misc1 => misc1;
+        public int Misc2 => misc2;
+    }
+}
