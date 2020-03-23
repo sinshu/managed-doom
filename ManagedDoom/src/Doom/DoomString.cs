ec826ee5bf5f4703ee7b4a9a7249fa4f10186ec6
sinshu <sinshu.d@gmail.com>
2020-03-23 21:17:51 +0900

    Add a special string type for DeHackEd support.

---
+++ ManagedDoom/src/Doom/DoomString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ManagedDoom
+{
+    public sealed class DoomString
+    {
+        private static Dictionary<string, DoomString> table = new Dictionary<string, DoomString>();
+
+        private string original;
+        private string replaced;
+
+        public DoomString(string original)
+        {
+            this.original = original;
+            replaced = original;
+            table.Add(original, this);
+        }
+
+        public override string ToString()
+        {
+            return replaced;
+        }
+
+        public static implicit operator string(DoomString ds)
+        {
+            return ds.replaced;
+        }
+
+        public static void Replace(string original, string replaced)
+        {
+            DoomString ds;
+            if (table.TryGetValue(original, out ds))
+            {
+                ds.replaced = replaced;
+            }
+        }
+    }
+}
