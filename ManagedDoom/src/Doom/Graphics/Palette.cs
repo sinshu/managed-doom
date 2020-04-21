226a7da789a1c2112967924e72c50c052ba756a8
sinshu <sinshu.d@gmail.com>
2020-04-21 17:20:38 +0900

    Add gamma correction (experimental).

--- ManagedDoom/src/Doom/Graphics/Palette.cs
+++ ManagedDoom/src/Doom/Graphics/Palette.cs
@@ -9,6 +9,33 @@ namespace ManagedDoom
         public Palette(Wad wad)
         {
             data = wad.ReadLump("PLAYPAL");
+
+            GammaCorrectionTest();
+        }
+
+        private void GammaCorrectionTest()
+        {
+            for (var i = 0; i < 256; i++)
+            {
+                var offset = 3 * i;
+
+                var r = data[offset];
+                var g = data[offset + 1];
+                var b = data[offset + 2];
+
+                r = (byte)Math.Round(255 * CorrectionCurve(r / 255.0));
+                g = (byte)Math.Round(255 * CorrectionCurve(g / 255.0));
+                b = (byte)Math.Round(255 * CorrectionCurve(b / 255.0));
+
+                data[offset] = r;
+                data[offset + 1] = g;
+                data[offset + 2] = b;
+            }
+        }
+
+        public static double CorrectionCurve(double x)
+        {
+            return Math.Pow(x, 0.9);
         }
 
         public byte[] Data => data;
