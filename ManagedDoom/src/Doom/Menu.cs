6edac18676adb3eda0a595a53d08a2335f85ee9f
sinshu <sinshu.d@gmail.com>
2020-05-05 03:18:45 +0900

    Add some menu related code.

---
+++ ManagedDoom/src/Doom/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ManagedDoom
+{
+    public sealed class Menu
+    {
+        // # of menu items
+        public int NumItems;
+
+        // previous menu
+        public Menu PrevMenu;
+
+        // menu items
+        public MenuItem MenuItems;
+
+        // draw routine
+        public Action Routine;
+
+        // x,y of menu
+        public int X;
+        public int Y;
+
+        // last item user was on in menu
+        public int LastOn;
+    }
+}
---
+++ ManagedDoom/src/Doom/MenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManagedDoom
+{
+    public sealed class MenuItem
+    {
+        // 0 = no cursor here, 1 = ok, 2 = arrows ok
+        public short Status;
+
+        public string Name;
+
+        // choice = menu item #.
+        // if status = 2,
+        //   choice = 0: leftarrow,
+        //            1: rightarrow
+        public Action<int> Routine;
+
+        // hotkey in menu
+        public char AlphaKey;
+    }
+}
