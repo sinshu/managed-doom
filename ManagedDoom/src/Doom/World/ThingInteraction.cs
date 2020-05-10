1a106cb3f2dcc4f6dd26408b5efc55ad049c2e18
sinshu <sinshu.d@gmail.com>
2020-05-10 23:25:58 +0900

    Refactor some gaming code.

--- ManagedDoom/src/Doom/World/ThingInteraction.cs
+++ ManagedDoom/src/Doom/World/ThingInteraction.cs
@@ -9,6 +9,8 @@ namespace ManagedDoom
 		public ThingInteraction(World world)
 		{
 			this.world = world;
+
+			InitRadiusAttack();
 		}
 
 		//
@@ -28,7 +30,7 @@ namespace ManagedDoom
 
 			if (source != null && source.Player != null)
 			{
-				// count for intermission
+				// Count for intermission.
 				if ((target.Flags & MobjFlags.CountKill) != 0)
 				{
 					source.Player.KillCount++;
@@ -36,19 +38,18 @@ namespace ManagedDoom
 
 				if (target.Player != null)
 				{
-					//source.Player.Frags[target->player - players]++;
+					source.Player.Frags[target.Player.Number]++;
 				}
 			}
 			else if (!world.Options.NetGame && (target.Flags & MobjFlags.CountKill) != 0)
 			{
-				// count all monster deaths,
-				// even those caused by other monsters
+				// Count all monster deaths, even those caused by other monsters.
 				world.Players[0].KillCount++;
 			}
 
 			if (target.Player != null)
 			{
-				// count environment kills against you
+				// Count environment kills against you.
 				if (source == null)
 				{
 					target.Player.Frags[target.Player.Number]++;
@@ -69,8 +70,7 @@ namespace ManagedDoom
 				*/
 			}
 
-			if (target.Health < -target.Info.SpawnHealth
-				&& target.Info.XdeathState != 0)
+			if (target.Health < -target.Info.SpawnHealth && target.Info.XdeathState != 0)
 			{
 				target.SetState(target.Info.XdeathState);
 			}
@@ -85,12 +85,8 @@ namespace ManagedDoom
 				target.Tics = 1;
 			}
 
-			//	I_StartSound (&actor->r, actor->info->deathsound);
-
-
 			// Drop stuff.
-			// This determines the kind of object spawned
-			// during the death frame of a thing.
+			// This determines the kind of object spawned during the death frame of a thing.
 			MobjType item;
 			switch (target.Type)
 			{
@@ -112,41 +108,19 @@ namespace ManagedDoom
 			}
 
 			var mo = world.ThingAllocation.SpawnMobj(target.X, target.Y, Mobj.OnFloorZ, item);
-			mo.Flags |= MobjFlags.Dropped; // special versions of items
+
+			// Special versions of items.
+			mo.Flags |= MobjFlags.Dropped;
 		}
 
 
+		private static readonly int baseThreshold = 100;
 
-
-
-
-
-
-		private int BASETHRESHOLD = 100;
-
-
-		//
-		// P_DamageMobj
-		// Damages both enemies and players
-		// "inflictor" is the thing that caused the damage
-		//  creature or missile, can be NULL (slime, etc)
-		// "source" is the thing to target after taking damage
-		//  creature or NULL
-		// Source and inflictor are the same for melee attacks.
-		// Source can be NULL for slime, barrel explosions
-		// and other environmental stuff.
-		//
-		public void DamageMobj(
-			Mobj target,
-			Mobj inflictor,
-			Mobj source,
-			int damage)
+		public void DamageMobj(Mobj target, Mobj inflictor, Mobj source, int damage)
 		{
-			int temp;
-
 			if ((target.Flags & MobjFlags.Shootable) == 0)
 			{
-				// shouldn't happen...
+				// Shouldn't happen...
 				return;
 			}
 
@@ -163,18 +137,18 @@ namespace ManagedDoom
 			var player = target.Player;
 			if (player != null && world.Options.Skill == GameSkill.Baby)
 			{
-				// take half damage in trainer mode
+				// Take half damage in trainer mode.
 				damage >>= 1;
 			}
 
-			// Some close combat weapons should not
-			// inflict thrust and push the victim out of reach,
+			// Some close combat weapons should not inflict thrust and push the victim out of reach,
 			// thus kick away unless using the chainsaw.
-			if (inflictor != null
-				&& (target.Flags & MobjFlags.NoClip) == 0
-				&& (source == null
-					|| source.Player == null
-					|| source.Player.ReadyWeapon != WeaponType.Chainsaw))
+			var notChainsawAttack =
+				source == null ||
+				source.Player == null ||
+				source.Player.ReadyWeapon != WeaponType.Chainsaw;
+
+			if (inflictor != null && (target.Flags & MobjFlags.NoClip) == 0 && notChainsawAttack)
 			{
 				var ang = Geometry.PointToAngle(
 					inflictor.X,
@@ -184,40 +158,38 @@ namespace ManagedDoom
 
 				var thrust = new Fixed(damage * (Fixed.FracUnit >> 3) * 100 / target.Info.Mass);
 
-				// make fall forwards sometimes
-				if (damage < 40
-					&& damage > target.Health
-					&& target.Z - inflictor.Z > Fixed.FromInt(64)
-					&& (world.Random.Next() & 1) != 0)
+				// Make fall forwards sometimes.
+				if (damage < 40 &&
+					damage > target.Health &&
+					target.Z - inflictor.Z > Fixed.FromInt(64) &&
+					(world.Random.Next() & 1) != 0)
 				{
 					ang += Angle.Ang180;
 					thrust *= 4;
 				}
 
-				//ang >>= ANGLETOFINESHIFT;
-				target.MomX += thrust * Trig.Cos(ang); // finecosine[ang]);
-				target.MomY += thrust * Trig.Sin(ang); // finesine[ang]);
+				target.MomX += thrust * Trig.Cos(ang);
+				target.MomY += thrust * Trig.Sin(ang);
 			}
 
-			// player specific
+			// Player specific.
 			if (player != null)
 			{
-				// end of game hell hack
-				if (target.Subsector.Sector.Special == (SectorSpecial)11
-					&& damage >= target.Health)
+				// End of game hell hack.
+				if (target.Subsector.Sector.Special == (SectorSpecial)11 && damage >= target.Health)
 				{
 					damage = target.Health - 1;
 				}
 
-				// Below certain threshold,
-				// ignore damage in GOD mode, or with INVUL power.
-				if (damage < 1000 && ((player.Cheats & CheatFlags.GodMode) != 0
-					|| player.Powers[(int)PowerType.Invulnerability] > 0))
+				// Below certain threshold, ignore damage in GOD mode, or with INVUL power.
+				if (damage < 1000 && ((player.Cheats & CheatFlags.GodMode) != 0 ||
+					player.Powers[(int)PowerType.Invulnerability] > 0))
 				{
 					return;
 				}
 
 				int saved;
+
 				if (player.ArmorType != 0)
 				{
 					if (player.ArmorType == 1)
@@ -225,19 +197,22 @@ namespace ManagedDoom
 						saved = damage / 3;
 					}
 					else
+					{
 						saved = damage / 2;
+					}
 
 					if (player.ArmorPoints <= saved)
 					{
-						// armor is used up
+						// Armor is used up.
 						saved = player.ArmorPoints;
 						player.ArmorType = 0;
 					}
+
 					player.ArmorPoints -= saved;
 					damage -= saved;
 				}
 
-				// mirror mobj health here for Dave
+				// Mirror mobj health here for Dave.
 				player.Health -= damage;
 				if (player.Health < 0)
 				{
@@ -246,26 +221,17 @@ namespace ManagedDoom
 
 				player.Attacker = source;
 
-				// add damage after armor / invuln
+				// Add damage after armor / invuln.
 				player.DamageCount += damage;
 
 				if (player.DamageCount > 100)
 				{
-					// teleport stomp does 10k points...
+					// Teleport stomp does 10k points...
 					player.DamageCount = 100;
 				}
-
-				temp = damage < 100 ? damage : 100;
-
-				/*
-				if (player == &players[consoleplayer])
-				{
-					I_Tactile(40, 10, 40 + temp * 2);
-				}
-				*/
 			}
 
-			// do the damage	
+			// Do the damage	.
 			target.Health -= damage;
 			if (target.Health <= 0)
 			{
@@ -273,39 +239,33 @@ namespace ManagedDoom
 				return;
 			}
 
-			if ((world.Random.Next() < target.Info.PainChance)
-				&& (target.Flags & MobjFlags.SkullFly) == 0)
+			if ((world.Random.Next() < target.Info.PainChance) &&
+				(target.Flags & MobjFlags.SkullFly) == 0)
 			{
-				// fight back!
+				// Fight back!
 				target.Flags |= MobjFlags.JustHit;
 
 				target.SetState(target.Info.PainState);
 			}
 
-			// we're awake now...
+			// We're awake now...
 			target.ReactionTime = 0;
 
-			if ((target.Threshold == 0 || target.Type == MobjType.Vile)
-				&& source != null && source != target
-				&& source.Type != MobjType.Vile)
+			if ((target.Threshold == 0 || target.Type == MobjType.Vile) &&
+				source != null && source != target &&
+				source.Type != MobjType.Vile)
 			{
-				// if not intent on another player,
-				// chase after this one
+				// If not intent on another player, chase after this one.
 				target.Target = source;
-				target.Threshold = BASETHRESHOLD;
-				if (target.State == DoomInfo.States[(int)target.Info.SpawnState]
-					&& target.Info.SeeState != MobjState.Null)
+				target.Threshold = baseThreshold;
+				if (target.State == DoomInfo.States[(int)target.Info.SpawnState] &&
+					target.Info.SeeState != MobjState.Null)
 				{
 					target.SetState(target.Info.SeeState);
 				}
 			}
 		}
 
-
-
-		//
-		// P_ExplodeMissile  
-		//
 		public void ExplodeMissile(Mobj thing)
 		{
 			thing.MomX = thing.MomY = thing.MomZ = Fixed.Zero;
@@ -328,36 +288,32 @@ namespace ManagedDoom
 		}
 
 
+		private Mobj bombSource;
+		private Mobj bombSpot;
+		private int bombDamage;
 
+		private Func<Mobj, bool> radiusAttackFunc;
 
-		//
-		// RADIUS ATTACK
-		//
-		private Mobj bombsource;
-		private Mobj bombspot;
-		private int bombdamage;
+		private void InitRadiusAttack()
+		{
+			radiusAttackFunc = RadiusAttack;
+		}
 
-		//
-		// PIT_RadiusAttack
-		// "bombsource" is the creature
-		// that caused the explosion at "bombspot".
-		//
-		private bool PIT_RadiusAttack(Mobj thing)
+		private bool RadiusAttack(Mobj thing)
 		{
 			if ((thing.Flags & MobjFlags.Shootable) == 0)
 			{
 				return true;
 			}
 
-			// Boss spider and cyborg
-			// take no damage from concussion.
+			// Boss spider and cyborg take no damage from concussion.
 			if (thing.Type == MobjType.Cyborg || thing.Type == MobjType.Spider)
 			{
 				return true;
 			}
 
-			var dx = Fixed.Abs(thing.X - bombspot.X);
-			var dy = Fixed.Abs(thing.Y - bombspot.Y);
+			var dx = Fixed.Abs(thing.X - bombSpot.X);
+			var dy = Fixed.Abs(thing.Y - bombSpot.Y);
 
 			var dist = dx > dy ? dx : dy;
 			dist = new Fixed((dist - thing.Radius).Data >> Fixed.FracBits);
@@ -367,42 +323,38 @@ namespace ManagedDoom
 				dist = Fixed.Zero;
 			}
 
-			if (dist.Data >= bombdamage)
+			if (dist.Data >= bombDamage)
 			{
-				// out of range
+				// Out of range.
 				return true;
 			}
 
-			if (world.VisibilityCheck.CheckSight(thing, bombspot))
+			if (world.VisibilityCheck.CheckSight(thing, bombSpot))
 			{
-				// must be in direct path
-				DamageMobj(thing, bombspot, bombsource, bombdamage - dist.Data);
+				// Must be in direct path.
+				DamageMobj(thing, bombSpot, bombSource, bombDamage - dist.Data);
 			}
 
 			return true;
 		}
 
-		//
-		// P_RadiusAttack
-		// Source is the creature that caused the explosion at spot.
-		//
 		public void RadiusAttack(Mobj spot, Mobj source, int damage)
 		{
 			var bm = world.Map.BlockMap;
 			var dist = Fixed.FromInt(damage + GameConstants.MaxThingRadius.Data);
-			var yh = (spot.Y + dist - bm.OriginY).Data >> BlockMap.MapBlockShift;
-			var yl = (spot.Y - dist - bm.OriginY).Data >> BlockMap.MapBlockShift;
-			var xh = (spot.X + dist - bm.OriginX).Data >> BlockMap.MapBlockShift;
-			var xl = (spot.X - dist - bm.OriginX).Data >> BlockMap.MapBlockShift;
-			bombspot = spot;
-			bombsource = source;
-			bombdamage = damage;
+			var blockY1 = bm.GetBlockY(spot.Y - dist);
+			var blockY2 = bm.GetBlockY(spot.Y + dist);
+			var blockX1 = bm.GetBlockX(spot.X - dist);
+			var blockX2 = bm.GetBlockX(spot.X + dist);
+			bombSpot = spot;
+			bombSource = source;
+			bombDamage = damage;
 
-			for (var y = yl; y <= yh; y++)
+			for (var by = blockY1; by <= blockY2; by++)
 			{
-				for (var x = xl; x <= xh; x++)
+				for (var bx = blockX1; bx <= blockX2; bx++)
 				{
-					bm.IterateThings(x, y, mo => PIT_RadiusAttack(mo));
+					bm.IterateThings(bx, by, radiusAttackFunc);
 				}
 			}
 		}
