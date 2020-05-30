fa82777ef596cf11704eb08e1f3ed4e376e59861
sinshu <sinshu.d@gmail.com>
2020-05-30 12:07:06 +0900

    Do intermission animation.

---
+++ ManagedDoom/src/Doom/Intermission/WorldMap.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ManagedDoom
+{
+    public static class WorldMap
+    {
+        public static readonly IReadOnlyList<IReadOnlyList<Point>> Locations = new Point[][]
+        {
+            new Point[]
+            {
+                new Point(185, 164), // location of level 0 (CJ)
+	            new Point(148, 143), // location of level 1 (CJ)
+	            new Point(69, 122),  // location of level 2 (CJ)
+	            new Point(209, 102), // location of level 3 (CJ)
+	            new Point(116, 89),  // location of level 4 (CJ)
+	            new Point(166, 55),  // location of level 5 (CJ)
+	            new Point(71, 56),   // location of level 6 (CJ)
+	            new Point(135, 29),  // location of level 7 (CJ)
+	            new Point(71, 24)    // location of level 8 (CJ)
+            },
+
+            new Point[]
+            {
+                new Point(254, 25),  // location of level 0 (CJ)
+	            new Point(97, 50),   // location of level 1 (CJ)
+	            new Point(188, 64),  // location of level 2 (CJ)
+	            new Point(128, 78),  // location of level 3 (CJ)
+	            new Point(214, 92),  // location of level 4 (CJ)
+	            new Point(133, 130), // location of level 5 (CJ)
+	            new Point(208, 136), // location of level 6 (CJ)
+	            new Point(148, 140), // location of level 7 (CJ)
+	            new Point(235, 158)  // location of level 8 (CJ)
+            },
+
+            new Point[]
+            {
+                new Point(156, 168), // location of level 0 (CJ)
+                new Point(48, 154),  // location of level 1 (CJ)
+                new Point(174, 95),  // location of level 2 (CJ)
+                new Point(265, 75),  // location of level 3 (CJ)
+                new Point(130, 48),  // location of level 4 (CJ)
+                new Point(279, 23),  // location of level 5 (CJ)
+                new Point(198, 48),  // location of level 6 (CJ)
+                new Point(140, 25),  // location of level 7 (CJ)
+                new Point(281, 136)  // location of level 8 (CJ)
+            }
+        };
+
+
+
+        public class Point
+        {
+            private int x;
+            private int y;
+
+            public Point(int x, int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+
+            public int X => x;
+            public int Y => y;
+        }
+    }
+}
