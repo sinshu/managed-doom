d81660164eed156d154f288d4775bc2a0b125d9b
sinshu <sinshu.d@gmail.com>
2020-06-08 19:37:43 +0900

    Do save / load.

---
+++ ManagedDoom/src/Doom/Game/SaveLoad.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ManagedDoom
+{
+    public static class SaveLoad
+    {
+        private enum ThinkerClass
+        {
+            End,
+            Mobj
+        }
+
+        private enum Specials
+        {
+            Ceiling,
+            Door,
+            Floor,
+            Plat,
+            Flash,
+            Strobe,
+            Glow,
+            EndSpecials
+        }
+    }
+}
