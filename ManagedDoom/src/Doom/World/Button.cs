79a940835063a33b0dbf18080db7907da8c071f3
sinshu <sinshu.d@gmail.com>
2020-07-11 15:46:00 +0900

    Do refactoring.

--- ManagedDoom/src/Doom/World/Button.cs
+++ ManagedDoom/src/Doom/World/Button.cs
@@ -4,19 +4,49 @@ namespace ManagedDoom
 {
     public sealed class Button
     {
-        public LineDef Line;
-        public ButtonPosition Position;
-        public int Texture;
-        public int Timer;
-        public Mobj SoundOrigin;
+        private LineDef line;
+        private ButtonPosition position;
+        private int texture;
+        private int timer;
+        private Mobj soundOrigin;
 
         public void Clear()
         {
-            Line = null;
-            Position = 0;
-            Texture = 0;
-            Timer = 0;
-            SoundOrigin = null;
+            line = null;
+            position = 0;
+            texture = 0;
+            timer = 0;
+            soundOrigin = null;
+        }
+
+        public LineDef Line
+        {
+            get => line;
+            set => line = value;
+        }
+
+        public ButtonPosition Position
+        {
+            get => position;
+            set => position = value;
+        }
+
+        public int Texture
+        {
+            get => texture;
+            set => texture = value;
+        }
+
+        public int Timer
+        {
+            get => timer;
+            set => timer = value;
+        }
+
+        public Mobj SoundOrigin
+        {
+            get => soundOrigin;
+            set => soundOrigin = value;
         }
     }
 }
