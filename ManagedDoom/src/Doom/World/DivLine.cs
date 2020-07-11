544f4ef0d53ed82fe85610ffd161a331de5c076c
sinshu <sinshu.d@gmail.com>
2020-07-11 17:17:04 +0900

    Do refactoring.

--- ManagedDoom/src/Doom/World/DivLine.cs
+++ ManagedDoom/src/Doom/World/DivLine.cs
@@ -4,17 +4,41 @@ namespace ManagedDoom
 {
     public sealed class DivLine
     {
-        public Fixed X;
-        public Fixed Y;
-        public Fixed Dx;
-        public Fixed Dy;
+        private Fixed x;
+        private Fixed y;
+        private Fixed dx;
+        private Fixed dy;
 
         public void MakeFrom(LineDef line)
         {
-            X = line.Vertex1.X;
-            Y = line.Vertex1.Y;
-            Dx = line.Dx;
-            Dy = line.Dy;
+            x = line.Vertex1.X;
+            y = line.Vertex1.Y;
+            dx = line.Dx;
+            dy = line.Dy;
+        }
+
+        public Fixed X
+        {
+            get => x;
+            set => x = value;
+        }
+
+        public Fixed Y
+        {
+            get => y;
+            set => y = value;
+        }
+
+        public Fixed Dx
+        {
+            get => dx;
+            set => dx = value;
+        }
+
+        public Fixed Dy
+        {
+            get => dy;
+            set => dy = value;
         }
     }
 }
