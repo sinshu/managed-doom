6373fc79f8fd85468df6b6d99842ab88bcc6bfd2
sinshu <sinshu.d@gmail.com>
2020-07-12 13:39:26 +0900

    Do refactoring.

--- ManagedDoom/src/Doom/World/MobjFlags.cs
+++ ManagedDoom/src/Doom/World/MobjFlags.cs
@@ -17,7 +17,7 @@ namespace ManagedDoom
         // Don't use the sector links (invisible but touchable).
         NoSector = 8,
 
-        // Don't use the blocklinks (inert but displayable)
+        // Don't use the blocklinks (inert but displayable).
         NoBlockMap = 16,
 
         // Not to be activated by sound, deaf monster.
@@ -30,12 +30,12 @@ namespace ManagedDoom
         JustAttacked = 128,
 
         // On level spawning (initial position),
-        //  hang from ceiling instead of stand on floor.
+        // hang from ceiling instead of stand on floor.
         SpawnCeiling = 256,
 
         // Don't apply gravity (every tic),
-        //  that is, object will float, keeping current height
-        //  or changing it actively.
+        // that is, object will float, keeping current height
+        // or changing it actively.
         NoGravity = 512,
 
         // Movement flags.
@@ -56,7 +56,7 @@ namespace ManagedDoom
         Float = 0x4000,
 
         // Don't cross lines
-        //   ??? or look at heights on teleport.
+        // ??? or look at heights on teleport.
         Teleport = 0x8000,
 
         // Don't hit same species, explode on block.
@@ -68,28 +68,28 @@ namespace ManagedDoom
         Dropped = 0x20000,
 
         // Use fuzzy draw (shadow demons or spectres),
-        //  temporary player invisibility powerup.
+        // temporary player invisibility powerup.
         Shadow = 0x40000,
 
         // Flag: don't bleed when shot (use puff),
-        //  barrels and shootable furniture shall not bleed.
+        // barrels and shootable furniture shall not bleed.
         NoBlood = 0x80000,
 
         // Don't stop moving halfway off a step,
-        //  that is, have dead bodies slide down all the way.
+        // that is, have dead bodies slide down all the way.
         Corpse = 0x100000,
 
         // Floating to a height for a move, ???
-        //  don't auto float to target's height.
+        // don't auto float to target's height.
         InFloat = 0x200000,
 
         // On kill, count this enemy object
-        //  towards intermission kill total.
+        // towards intermission kill total.
         // Happy gathering.
         CountKill = 0x400000,
 
         // On picking up, count this item object
-        //  towards intermission item total.
+        // towards intermission item total.
         CountItem = 0x800000,
 
         // Special handling: skull in flight.
@@ -97,13 +97,13 @@ namespace ManagedDoom
         SkullFly = 0x1000000,
 
         // Don't spawn this object
-        //  in death match mode (e.g. key cards).
+        // in death match mode (e.g. key cards).
         NotDeathmatch = 0x2000000,
 
         // Player sprites in multiplayer modes are modified
-        //  using an internal color lookup table for re-indexing.
+        // using an internal color lookup table for re-indexing.
         // If 0x4 0x8 or 0xc,
-        //  use a translation table for player colormaps
+        // use a translation table for player colormaps
         Translation = 0xc000000,
 
         // Hmm ???.
--- ManagedDoom/src/Doom/World/MobjStateDef.cs
+++ ManagedDoom/src/Doom/World/MobjStateDef.cs
@@ -4,15 +4,15 @@ namespace ManagedDoom
 {
     public class MobjStateDef
     {
-        public int Number;
-        public Sprite Sprite;
-        public int Frame;
-        public int Tics;
-        public Action<World, Player, PlayerSpriteDef> PlayerAction;
-        public Action<World, Mobj> MobjAction;
-        public MobjState Next;
-        public int Misc1;
-        public int Misc2;
+        private int number;
+        private Sprite sprite;
+        private int frame;
+        private int tics;
+        private Action<World, Player, PlayerSpriteDef> playerAction;
+        private Action<World, Mobj> mobjAction;
+        private MobjState next;
+        private int misc1;
+        private int misc2;
 
         public MobjStateDef(
             int number,
@@ -25,17 +25,25 @@ namespace ManagedDoom
             int misc1,
             int misc2)
         {
-            Number = number;
-
-            Sprite = sprite;
-            Frame = frame;
-            Tics = tics;
-            PlayerAction = playerAction;
-            MobjAction = mobjAction;
-
-            Next = next;
-            Misc1 = misc1;
-            Misc2 = misc2;
+            this.number = number;
+            this.sprite = sprite;
+            this.frame = frame;
+            this.tics = tics;
+            this.playerAction = playerAction;
+            this.mobjAction = mobjAction;
+            this.next = next;
+            this.misc1 = misc1;
+            this.misc2 = misc2;
         }
+
+        public int Number => number;
+        public Sprite Sprite => sprite;
+        public int Frame => frame;
+        public int Tics => tics;
+        public Action<World, Player, PlayerSpriteDef> PlayerAction => playerAction;
+        public Action<World, Mobj> MobjAction => mobjAction;
+        public MobjState Next => next;
+        public int Misc1 => misc1;
+        public int Misc2 => misc2;
     }
 }
