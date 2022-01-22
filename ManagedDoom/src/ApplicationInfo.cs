895ec9eeab371da3cca32099745a20cee0c43544
田中信秋 <sinshu.d@gmail.com>
2022-01-23 01:01:26 +0900

    Prepare for release.

--- ManagedDoom/src/ApplicationInfo.cs
+++ ManagedDoom/src/ApplicationInfo.cs
@@ -21,6 +21,6 @@ namespace ManagedDoom
 {
     public static class ApplicationInfo
     {
-        public static readonly string Title = "Managed Doom v1.2e";
+        public static readonly string Title = "Managed Doom v1.2f";
     }
 }
